7670c4abdee044291ea028d76c972efab3c346a4
emilHof <95590295+emilHof@users.noreply.github.com>
2022-02-06 23:24:30 -0500

    added additional motherboards

--- test.m
+++ test.m
@@ -4,8 +4,10 @@ RFM96w433mhz = radio(1.2, 95, 16, 115.12);
 xBeeProSX = radio(.0025, 55, 40, 120);
 
 %motherboards
-odriodC4 = motherboard(4, 2, 1, 0, 1, 1, 1, 4, 2.016, 157.33, 260, 50)
-rockPro64 = motherboard(4, 1, 2, 1, 0, 1, 1, 6, 1.533, 185, 650,80)
+odroidC4 = motherboard(4, 2, 1, 0, 1, 1, 1, 4, 2.016, 157.33, 260, 50);
+rockPro64 = motherboard(4, 1, 2, 1, 0, 1, 1, 6, 1.533, 185, 650, 80);
+odroidN2plus = motherboard(4, 4, 0, 0, 1, 1, 1, 6, 2.667, 150, 516, 80);
+pineRock64 = motherboard(2, 1, 2, 0, 0, 1, 1, 4, 1.100, 1.542, 129, 346, 55); 
 
 newBattery = battery(1000, 50);
 newDrive = hardDrive(256);
